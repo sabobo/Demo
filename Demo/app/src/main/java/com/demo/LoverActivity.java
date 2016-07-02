edb00cca111b2ee020f3c1adff89b669e0dc7ec8
sabobo <906621525@qq.com>
2016-07-02 16:46:02 +0800

    2016-07-02
    第一次提交

---
+++ Demo/app/src/main/java/com/demo/LoverActivity.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_love);
+    }
+
+}
